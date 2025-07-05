7262fd0a7855ee0905e99990e93cc28e388faf6a
Lena_Bondar <lencha.2005@gmail.com>
2025-07-05 16:41:57 +0300

    create Header, Icon, sprite

---
+++ src/components/ui/Header/header.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import React from "react";
+import Image from "next/image";
+import Icon from "@/components/elements/icon";
+
+import styles from "./header.module.css";
+import clsx from 'clsx';
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const buildCssClasses = ({ isActive }: { isActive: boolean }) =>
+  clsx(styles.link, isActive && styles.activeLink);
+
+export default function Header() {
+  const pathname = usePathname();
+  return (
+    <header className={styles.header}>
+      <Image
+        src="/images/logo-mobile.png"
+        alt="Logo"
+        width={40}
+        height={40}
+        className={styles.imgMob}
+      />
+      <Image
+        src="/images/logo-tablet.png"
+        alt="Logo"
+        width={56}
+        height={56}
+        className={styles.imgTablet}
+      />
+      <Image
+        src="/images/logo-desktop.png"
+        alt="Logo"
+        width={64}
+        height={64}
+        className={styles.imgDesktop}
+      />
+      <div className={styles.wrapperNav}>
+      <div className={styles.linkWrapper}>
+      <Link
+        href="/login"
+        className={buildCssClasses({ isActive: pathname === '/login' })}
+      >
+        Увійти
+      </Link>
+      <Link
+        href="/register"
+        className={buildCssClasses({ isActive: pathname === '/register' || pathname === '/' })}
+      >
+        Зареєструватися
+      </Link>      </div>
+      <button className={styles.btnBurger}>
+        <Icon name="icon-burger-menu" className={styles.iconBurger} />
+      </button>
+      </div>
+    </header>
+  );
+}
+
