efd0dc36c5941a4727bb90d4a811e6b8a7f9b6ee
Lena_Bondar <lencha.2005@gmail.com>
2025-07-07 17:29:36 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,6 @@ import type { Metadata } from "next";
 import { Roboto, Lato, Poppins, Open_Sans, Inter} from "next/font/google";
 import "../styles/globals.css";
 import { sourceSansPro } from "@/fonts/fonts";
-import Header from "@/components/ui/Header/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
