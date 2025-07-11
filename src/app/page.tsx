2e1aeef0eb1d11df08284319549408f6476f6c9b
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2025-07-11 19:36:39 +0200

    reviews store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Description from "@/components/ui/Description";
+import homeMetadata from "@/metadata/homeMetadata";
+import Description from "@/components/ui/Description/Description";
 import FAQ from "@/components/ui/FAQ/FAQ";
 import Reviews from "@/components/ui/Reviews/Reviews";
-import History from "@/components/ui/History";
+import History from "@/components/ui/History/History";
+
+export const metadata = homeMetadata;
 
 export default function page() {
   return (
