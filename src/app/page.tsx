0488527da1eea3f4cd9da4cd16eb621f2bdd3160
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2025-07-14 17:51:38 +0200

    fix conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,15 @@ import FAQ from "@/components/ui/FAQ/FAQ";
 import Reviews from "@/components/ui/Reviews/Reviews";
 import History from "@/components/ui/History/History";
 import Certificates from "@/components/ui/Certificates/Certificates";
+import Category from "@/components/ui/Category/Category";
 
 export const metadata = homeMetadata;
 
 export default function page() {
   return (
     <div className="container">
-      <Category/>
+      <Description />
+      <Category />
       <History />
       <Certificates />
       <FAQ />
