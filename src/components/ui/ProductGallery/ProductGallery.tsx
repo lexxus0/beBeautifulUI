99390276cbd3d7389722dcf190e03213238889a1
Bohdan Haponiuk <jlomka54@gmail.com>
2025-07-25 13:49:49 +0400

    in progress

--- src/components/ui/ProductGallery/ProductGallery.tsx
+++ src/components/ui/ProductGallery/ProductGallery.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { Product } from "@/types/types";
 import Image from "next/image";
 import React from "react";
-import Icon from "../../elements/Icons";
 
 import css from "@/components/ui/ProductGallery/ProductGallery.module.css";
+import ProductRating from "../ProductRating/ProductRating";
+import { useViewport } from "@/helpers/hooks/useViewport";
 
 export interface ProductGalleryProps {
   product: Product;
@@ -11,7 +13,7 @@ export interface ProductGalleryProps {
 
 const ProductGallery = ({ product }: ProductGalleryProps) => {
   const imageUrl: string = "https://picsum.photos/600";
-  const starArray = Array(5).fill(null, 0);
+  const { width } = useViewport();
 
   return (
     <div>
@@ -22,20 +24,7 @@ const ProductGallery = ({ product }: ProductGalleryProps) => {
         height={320}
         className="rounded-lg object-cover"
       />
-      <div className={css.starAssessment}>
-        {starArray.map((_, index) => {
-          return (
-            <Icon
-              key={index}
-              name="icon-star"
-              width={24}
-              height={24}
-              useGradient
-            />
-          );
-        })}
-        <p className={css.fidbackQuantity}>(0 відгуків)</p>
-      </div>
+      {width !== null && width < 744 ? <ProductRating /> : null}
     </div>
   );
 };
