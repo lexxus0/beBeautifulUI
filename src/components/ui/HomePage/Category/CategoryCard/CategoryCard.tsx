6d24ae8f6a3981aa77fcfb97df9e05aeb637d614
Stanislav <ssovelich@gmail.com>
2025-07-28 14:58:39 +0300

    fix styles for categoryCard

--- src/components/ui/HomePage/Category/CategoryCard/CategoryCard.tsx
+++ src/components/ui/HomePage/Category/CategoryCard/CategoryCard.tsx
@@ -10,57 +10,42 @@ const CategoryCard = ({
   href,
 }: CategoryCardProps) => {
   return (
-  //  <Link href={href} className={styles.card}>
-  //     <div className={styles.image}>
-  //       <div
-  //         className={`${styles.bg} ${styles.mobile}`}
-  //         style={{ backgroundImage: `url(${imageMobile})` }}
-  //       />
-  //       <div
-  //         className={`${styles.bg} ${styles.desktop}`}
-  //         style={{ backgroundImage: `url(${imageDesktop})` }}
-  //       />
+    <div className={styles.flipCard}>
+      <div className={styles.flipInner}>
+        {/* FRONT */}
+        <div className={`${styles.cardFace} ${styles.front}`}>
+          <div
+            className={`${styles.bg} ${styles.mobile}`}
+            style={{ backgroundImage: `url(${imageMobile})` }}
+          />
+          <div
+            className={`${styles.bg} ${styles.desktop}`}
+            style={{ backgroundImage: `url(${imageDesktop})` }}
+          />
+          <div className={styles.overlay}></div>
+          <div className={styles.title}>
+            <Link href={href}>{title}</Link>
+          </div>
+        </div>
 
-  //       <div className={styles.overlay}></div>
-  //       <div className={styles.title}>{title}</div>
-  //       <div className={styles.description}>{description}</div>
-  //     </div>
-  //   </Link>
-
-
-  <Link href={href} className={styles.flipCard}>
-  <div className={styles.flipInner}>
-    {/* FRONT */}
-    <div className={`${styles.cardFace} ${styles.front}`}>
-      <div
-        className={`${styles.bg} ${styles.mobile}`}
-        style={{ backgroundImage: `url(${imageMobile})` }}
-      />
-      <div
-        className={`${styles.bg} ${styles.desktop}`}
-        style={{ backgroundImage: `url(${imageDesktop})` }}
-      />
-      <div className={styles.overlay}></div>
-      <div className={styles.title}>{title}</div>
+        {/* BACK */}
+        <div className={`${styles.cardFace} ${styles.back}`}>
+          <div
+            className={`${styles.bg} ${styles.mobile}`}
+            style={{ backgroundImage: `url(${imageMobile})` }}
+          />
+          <div
+            className={`${styles.bg} ${styles.desktop}`}
+            style={{ backgroundImage: `url(${imageDesktop})` }}
+          />
+          <div className={styles.overlay}></div>
+          <div className={styles.title}>
+            <Link href={href}>{title}</Link>
+          </div>
+          <p className={styles.description}>{description}</p>
+        </div>
+      </div>
     </div>
-
-    {/* BACK */}
-    <div className={`${styles.cardFace} ${styles.back}`}>
-      <div
-        className={`${styles.bg} ${styles.mobile}`}
-        style={{ backgroundImage: `url(${imageMobile})` }}
-      />
-      <div
-        className={`${styles.bg} ${styles.desktop}`}
-        style={{ backgroundImage: `url(${imageDesktop})` }}
-      />
-      <div className={styles.overlay}></div>
-      <div className={`${styles.title} ${styles.titleHovered}`}>{title}</div>
-      <div className={styles.description}>{description}</div>
-    </div>
-  </div>
-</Link>
-
   );
 };
 
