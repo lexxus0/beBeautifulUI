2197cb0577166f62e57d2697420a0353f6d6a42b
denis713m <dvkarzov@gmail.com>
2025-08-03 14:24:29 +0300

    get rid of errors: hydration and keys

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,8 +1,16 @@
-"use client";
+'use client'
 
-import { store } from "@/store/store";
-import { Provider } from "react-redux";
+import { persistor, store } from '@/store/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  console.log('[CLIENT] Initial Redux state:', store.getState())
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 }
