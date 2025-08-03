87cda410d55669864655bcc43d4d5578029aeb50
denis713m <dvkarzov@gmail.com>
2025-08-03 14:30:59 +0300

    add old signs

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { persistor, store } from '@/store/store';
+import { persistor, store } from "@/store/store";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from "redux-persist/integration/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
