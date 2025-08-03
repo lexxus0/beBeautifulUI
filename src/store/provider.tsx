12918f8a61b8cd199418e0e11e187e8838cb0078
denis713m <dvkarzov@gmail.com>
2025-08-03 14:30:16 +0300

    add old signs

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,11 +1,10 @@
-"use client"
+"use client";
 
 import { persistor, store } from '@/store/store';
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  console.log('[CLIENT] Initial Redux state:', store.getState())
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
