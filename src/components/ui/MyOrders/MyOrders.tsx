844801c07d0730cd4b6d47b11d2c7fd56a15e61c
Lena_Bondar <lencha.2005@gmail.com>
2025-08-03 17:08:44 +0300

    create MyOrders

---
+++ src/components/ui/MyOrders/MyOrders.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { selectIsLoggedIn, selectUser } from "@/store/auth/selectors";
+import { useAppSelector } from "@/store/hooks";
+import { useRouter } from "next/navigation";
+import Icon from "@/components/shared/Icon";
+import OrderItem from "./OrderItem/OrderItem";
+
+import styles from "./MyOrders.module.scss";
+
+export default function MyOrders() {
+  const router = useRouter();
+  const user = useAppSelector(selectUser);
+  const isLoading = useAppSelector(selectIsLoggedIn);
+
+  useEffect(() => {
+    if (!user && !isLoading) {
+      router.push("/auth");
+    }
+  }, [user, isLoading, router]);
+
+  if (!user) {
+    return null; // або  <Loader />
+  }
+
+  return (
+    <div className="container pt-[10px] pb-15 md:pb-[42px] lg:pt-[34px] lg:b-15">
+      <div className="flex flex-col gap-4 mb-10 md:mb-[62px] md:flex-row items-center md:justify-between">
+        <h2 className="font-lato font-semibold text-2xl lg:text-[32px] text-[#49454f]">
+          Мої замовлення
+        </h2>
+        <div className="relative">
+          <input
+            type="text"
+            name="search"
+            className={styles.input}
+            placeholder="Пошук"
+          />
+          <Icon name="icon-search" className={styles.iconSearch} />
+        </div>
+      </div>
+      <p className={styles.text}>
+        Слідкуйте за статусом ваших замовлень у зручному форматі
+      </p>
+      <OrderItem />
+    </div>
+  );
+}
---
+++ src/components/ui/MyOrders/OrderItem/OrderItem.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+import styles from "./OrderItem.module.scss";
+
+// type Props = {}
+
+export default function OrderItem() {
+  return (
+    <>
+      {/* <div className="w-screen border-t border-t-gray-300"></div> */}
+      <div className="flex flex-col lg:gap-[74px] gap-10 lg:py-5 lg:mx-auto lg:w-[856px]">
+        <ul className={styles.listOrder}>
+          <li className={styles.itemOrder}>
+            <p className={styles.textOrder}>№ Замовлення:</p>
+            <span className={styles.spanOrder}>#SB00001</span>
+          </li>
+          <li className={styles.itemOrder}>
+            <p className={styles.textOrder}>Статус</p>
+            <span className={styles.spanOrder}>Доставлено</span>
+          </li>
+          <li className={styles.itemOrder}>
+            <p className={styles.textOrder}>Оплата</p>
+            <span className={styles.spanOrder}>Сплачено</span>
+          </li>
+          <li className={styles.itemOrder}>
+            <p className={styles.textOrder}>Дата</p>
+            <span className={styles.spanOrder}>21.07.2025</span>
+          </li>
+          <li className={styles.itemOrder}>
+            <p className={styles.textOrder}>Тип доставки</p>
+            <span className={styles.spanOrder}>Нова пошта</span>
+          </li>
+          <li className={styles.itemOrder}>
+            <p className={styles.textOrder}>Сума</p>
+            <span className={styles.spanOrder}>778 грн</span>
+          </li>
+        </ul>
+        <div>
+          <p className="font-lato font-semibold text-xl mb-5 md:font-bold md:text-lg">
+            Товари:
+          </p>
+          <ul className="flex flex-col gap-5 md:w-[500px]">
+            <li className="flex gap-4 items-center md:gap-6">
+              <p className="font-lato font-black text-lg">01</p>
+              <div className="flex flex-col gap-1 md:flex-row md:gap-9">
+                <p className="font-lato font-black text-lg">Shampoo</p>
+                <p className="font-light text-lg">Шампунь 300 мл</p>
+              </div>
+              <p className="ml-auto font-lato text-lg">1 шт</p>
+            </li>
+            <li className="flex gap-4 items-center md:gap-6">
+              <p className="font-lato font-black text-lg">02</p>
+              <div className="flex flex-col gap-1 md:flex-row md:gap-9">
+                <p className="font-lato font-black text-lg">Hair mask</p>
+                <p className="font-light text-lg">Маска для волосся 300 мл</p>
+              </div>
+              <p className="ml-auto font-lato text-lg">1 шт</p>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col pt-8 gap-6 md:flex-row md:gap-5 md:pt-10 lg:pt-15 lg:gap-6 relative">
+          <div className="w-screen h-px bg-gray-10 absolute left-1/2 -translate-x-1/2 top-0"></div>{" "}
+          <button type="button" onClick={() => {}} className={styles.btnOrder}>
+            Повторити замовлення
+          </button>
+          <button type="button" onClick={() => {}} className={styles.btnDetails}>
+            Детальніше
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
