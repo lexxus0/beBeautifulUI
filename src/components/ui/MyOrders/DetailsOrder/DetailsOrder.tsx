1504db392cfdb0cbcfa208348e86a6eccf7b9a3b
Lena_Bondar <lencha.2005@gmail.com>
2025-08-11 21:39:24 +0300

    Update DetailsOrder.tsx

--- src/components/ui/MyOrders/DetailsOrder/DetailsOrder.tsx
+++ src/components/ui/MyOrders/DetailsOrder/DetailsOrder.tsx
@@ -24,21 +24,21 @@ export default function DetailsOrder({ order }: IDetailsOrderProps) {
             <span className={styles.spanOrder}>{order?.number}</span>
           </li>
           <li>
-            <p className={styles.textOrder}>Дата:</p>
+            <p className={styles.textOrder}>Дата оформлення:</p>
             <span className={styles.spanOrder}>{order?.date}</span>
           </li>
-          <li>
+          {/* <li>
             <p className={styles.textOrder}>Статус:</p>
             <span className={styles.spanOrder}>{order?.status}</span>
-          </li>
+          </li> */}
           <li>
             <p className={styles.textOrder}>Спосіб доставки:</p>
             <span className={styles.spanOrder}>{order?.deliveryType}</span>
           </li>
-          <li>
+          {/* <li>
             <p className={styles.textOrder}>Номер ТТН:</p>
             <span className={styles.spanOrder}>{order?.ttn}</span>
-          </li>
+          </li> */}
           <li>
             <p className={styles.textOrder}>Спосіб оплати:</p>
             <span className={styles.spanOrder}>{order?.paymentType}</span>
