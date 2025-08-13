a8af4555ce85a5bfeb022d7ad6cf6c0805307b0d
Stanislav <ssovelich@gmail.com>
2025-08-13 14:00:38 +0300

    added component BackButton

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -8,6 +8,7 @@ import rawData from "./basket.json";
 import { useState, useMemo } from "react";
 import BasketIcon from "@/components/elements/BasketIcon";
 import RecommendedProducts from "@/components/ui/RecommendedProducts/RecommendedProducts";
+import BackButton from "@/components/ui/BackButton/BackButton";
 
 const BasketPage = () => {
   const [basketItems, setBasketItems] = useState<BasketItemType[]>(rawData);
@@ -41,34 +42,37 @@ const BasketPage = () => {
   );
 
   return (
-    <div className="container">
-      <div className={styles.basketPage}>
-        <div className={styles.basket}>
-          <h1 className={styles.title}>Кошик</h1>
-          <div className={styles.items}>
-            {basketItems.map((item) => (
-              <BasketItem
-                key={item.id}
-                item={item}
-                onIncrement={() => handleIncrement(item.id)}
-                onDecrement={() => handleDecrement(item.id)}
-                onRemove={() => handleRemove(item.id)}
-              />
-            ))}
+    <>
+      <BackButton />
+      <div className="container">
+        <div className={styles.basketPage}>
+          <div className={styles.basket}>
+            <h1 className={styles.title}>Кошик</h1>
+            <div className={styles.items}>
+              {basketItems.map((item) => (
+                <BasketItem
+                  key={item.id}
+                  item={item}
+                  onIncrement={() => handleIncrement(item.id)}
+                  onDecrement={() => handleDecrement(item.id)}
+                  onRemove={() => handleRemove(item.id)}
+                />
+              ))}
+            </div>
+            <hr className={styles.divider} />
+            <div className={styles.total}>
+              <span>Загальна сума:</span>
+              <span>{total} грн</span>
+            </div>
+            <Link href="/checkout" className={styles.checkoutBtn}>
+              Оформити замовлення{" "}
+              <BasketIcon variant="white" className={styles.iconBasket} />
+            </Link>
           </div>
-          <hr className={styles.divider} />
-          <div className={styles.total}>
-            <span>Загальна сума:</span>
-            <span>{total} грн</span>
-          </div>
-          <Link href="/checkout" className={styles.checkoutBtn}>
-            Оформити замовлення{" "}
-            <BasketIcon variant="white" className={styles.iconBasket} />
-          </Link>
+          <RecommendedProducts />
         </div>
-        <RecommendedProducts />
       </div>
-    </div>
+    </>
   );
 };
 
