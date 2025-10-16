825b708552a22b5f5586bc6fc07e0fa3bca8ee92
denis713m <dvkarzov@gmail.com>
2025-10-17 00:56:03 +0300

    speed loading product page

---
+++ src/components/ui/products/Product.tsx
@@ -0,0 +1,73 @@
+"use client"
+import { useEffect, useState } from 'react';
+import ProductActions from "@/components/ui/ProductActions/ProductActions";
+import ProductDescription from "@/components/ui/ProductDescription/ProductDescription";
+import ProductGallery from "@/components/ui/ProductGallery/ProductGallery";
+import ProductHeader from "@/components/ui/ProductHeader/ProductHeader";
+import { IProduct } from "@/types/types";
+import css from "./page.module.css";
+import BrandPhilosophy from "@/components/ui/BrandPhilosophy/BrandPhilosophy";
+import WantToKnowMore from "@/components/ui/WantToKnowMore/WantToKnowMore";
+import RecentlyViewed from "@/components/ui/RecentlyViewed/RecentlyViewed";
+import VisitedProduct from '@/app/products/[productId]/VisitedProduct';
+
+const getProductById = async (id: string): Promise<IProduct> => {
+  const res = await fetch(
+    `https://be-beautiful-backend.onrender.com/api/products/${id}`
+  );
+  const json = await res.json();
+  return json.data;
+};
+const Product = ({productId}: {productId: string}) => {
+
+  const [loading, setLoading] = useState(true)
+  const [product, setProduct] = useState<IProduct | null>(null)
+
+  useEffect(() => {
+    const getData = async () => {
+      if (productId) setProduct(await getProductById(productId))
+      setLoading(false)
+    }
+    getData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <section className="container">{
+      loading ? <span>Loading....</span>
+      :
+      (product && productId) ?
+            <section>
+        <VisitedProduct productId={productId} />
+        <div className={css.productContainer}>
+          <div className={css.header}>
+            <ProductHeader product={product} />
+          </div>
+          <div className={css.gallery}>
+            <ProductGallery product={product} />
+          </div>
+          <div className={css.description}>
+            <ProductDescription product={product} />
+          </div>
+          <div className={css.actions}>
+            <ProductActions product={product} />
+          </div>
+        </div>
+        <div className={css.fullWidthWrapper}>
+          <BrandPhilosophy dynamicText="Цей шампунь — як свіже «доброго ранку» собі. І як щоденне нагадування: ти — варта найкращого." />
+        </div>
+      </section>
+      :
+      <span>some problems...</span>
+    }
+      <section>
+      <RecentlyViewed />
+      </section>
+      <section>
+        <WantToKnowMore />
+      </section>
+    </section>
+  );
+}
+
+export default Product;
