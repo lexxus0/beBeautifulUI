bd9d52a8540fd638193c2dab3d155e5b26eb5bd4
Stanislav <ssovelich@gmail.com>
2025-10-27 16:32:37 +0200

    added logic for adding to favorites

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+"use client";
 
-// type FavoritesPageProps = {}
+import ProductItem from "@/components/ui/products/ProductItem";
+import { useAppSelector } from "@/store/hooks";
+import { IProduct } from "@/types/types";
 
 const FavoritesPage = () => {
-  return (
-    <div>Favorites Page</div>
-  )
-}
+  const favorites = useAppSelector((state) => state.favorites.items);
 
-export default FavoritesPage
+  if (favorites.length === 0)
+    return <p className="text-center mt-10">Список обраних порожній</p>;
+
+  return (
+    <div className="flex flex-wrap gap-10 justify-center my-10">
+      {favorites.map((item: IProduct) => (
+        <ProductItem key={item._id} item={item} />
+      ))}
+    </div>
+  );
+};
+
+export default FavoritesPage;
