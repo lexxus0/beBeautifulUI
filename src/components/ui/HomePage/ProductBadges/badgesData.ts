147f8676b1013083a8508add7ee20cdc3a9a2794
Stanislav <ssovelich@gmail.com>
2025-11-24 14:05:15 +0200

    added ProductBadges

---
+++ src/components/ui/HomePage/ProductBadges/badgesData.ts
@@ -0,0 +1,10 @@
+export const badges = [
+    { src: "/images/productBadges/balanced.png", alt: "Balanced icon" },
+    { src: "/images/productBadges/vegan.png", alt: "Vegan icon" },
+    {
+      src: "/images/productBadges/not-tested.png",
+      alt: "Not tested on animals icon",
+    },
+    { src: "/images/productBadges/recycling.png", alt: "Recycling icon" },
+    { src: "/images/productBadges/used-by.png", alt: "Used by icon" },
+  ];
