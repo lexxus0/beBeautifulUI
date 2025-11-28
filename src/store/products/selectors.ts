c64b7fffb5d5511053daded29d9283be949008d3
Lena_Bondar <lencha.2005@gmail.com>
2025-11-28 22:06:55 +0200

    fix product redux

--- src/store/products/selectors.ts
+++ src/store/products/selectors.ts
@@ -3,4 +3,6 @@ import { RootState } from "../store";
 export const selectProducts = (state: RootState) => state.products.products;
 export const selectRecentlyViewed = (state: RootState) => state.products.recentlyViewed;
 export const selectProductDetails = (state: RootState) => state.products.productDetails;
-export const selectIsLoading = (state: RootState) => state.products.isLoading;
+export const selectISsLoadingProduct = (state: RootState) => state.products.isLoadingProduct;
+export const selectIsLoadingRecently = (state: RootState) => state.products.isLoadingRecently;
+
